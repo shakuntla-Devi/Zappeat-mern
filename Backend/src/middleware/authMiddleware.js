fd1b3ccfce9e7bbfa7d17b3b8b6175d51423b9df
shakuntla Devi <sd2511825@gamil.com>
2026-04-20 19:21:15 +0530

    fixed auth middleware final

--- Backend/src/middleware/authMiddleware.js
+++ Backend/src/middleware/authMiddleware.js
@@ -1,14 +1,16 @@
 import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
 
-export const protect = (req, res, next) => {
+export const protect = async (req, res, next) => {
   try {
     let token;
+
     console.log("AUTH HEADER:", req.headers.authorization);
-    // 🔥 CHECK + EXTRACT TOKEN
+
+    // ✅ extract token
     if (
       req.headers.authorization &&
       req.headers.authorization.startsWith("Bearer")
-
     ) {
       token = req.headers.authorization.split(" ")[1];
     }
@@ -18,14 +20,21 @@ export const protect = (req, res, next) => {
       return res.status(401).json({ message: "No token ❌" });
     }
 
-    // 🔥 VERIFY
+    // ✅ verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    req.user = decoded;
+    // 🔥 IMPORTANT FIX (full user fetch)
+    const user = await User.findById(decoded.id).select("-password");
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found ❌" });
+    }
+
+    req.user = user; // ✅ now full user object
 
     next();
   } catch (error) {
-     console.log("JWT ERROR:", error.message); 
+    console.log("JWT ERROR:", error.message);
     res.status(401).json({ message: "Invalid token ❌" });
   }
 };
